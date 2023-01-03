657a09338cdc10847fcf5a94e6a8015610839926
Claudio Lau <claudio.lau12@gmail.com>
2023-01-03 09:07:19 -0500

    fixed dep error with asdf

--- bin/application.sh
+++ bin/application.sh
@@ -19,10 +19,16 @@ brew install asdf
 echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ~/.bash_profile
 echo -e "\n. $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash" >> ~/.bash_profile
 
+# add asdf exec to profile 
+echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ${ZDOTDIR:-~}/.zshrc
+
 # latest working version of node 
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf install nodejs 16.14.1
 asdf global nodejs 16.14.1
 
-# add asdf exec to profile 
-echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ${ZDOTDIR:-~}/.zshrc
+
+# go https://github.com/kennyp/asdf-golang
+asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+asdf reshim golang
+ASDF_GOLANG_OVERWRITE_ARCH=amd64 asdf install golang 1.15.8
--- bin/code-env.sh
+++ bin/code-env.sh
@@ -24,7 +24,3 @@ brew install htop
 # iac
 brew install terraform 
 
-# go https://github.com/kennyp/asdf-golang
-asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
-asdf reshim golang
-ASDF_GOLANG_OVERWRITE_ARCH=amd64 asdf install golang 1.15.8
